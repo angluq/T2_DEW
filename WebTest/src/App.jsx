c33a83fe042822906acb765864cfa0c9191a5e9a
angluq <79299906+angluq@users.noreply.github.com>
2024-08-17 16:58:46 +0000

    aplicando la página

--- WebTest/src/App.jsx
+++ WebTest/src/App.jsx
@@ -1,39 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import cibertec from './images/cibertec.jpg'
-import './App.css'
+import { Fragment } from "react";
+import Titulo from './componentes/Titulo';
+import Pie from './componentes/Pie'
+import Ciudad from './componentes/Ciudad'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.cibertec.edu.pe/" target="_blank">
-          <img src={cibertec} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Titulo />
+      <Ciudad
+        nombre="Lima"
+        descripcion="Capital del Perú"
+        imagen = "https://i.natgeofe.com/n/4b1eeacc-59c7-476e-8870-fadf135a998e/cathedral-lima-peru.jpg?w=2880&h=1912"
+      />
+      <Ciudad
+      nombre = "Arequipa"
+      descripcion="Encantadora tierra volcánica"
+      imagen = "https://www.peru.travel/contenido/destino/imagen/es/3/1.2/principal/plaza%20de%20armas%20arequipa.jpg"
+       />
+      <Pie />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- WebTest/src/componentes/Titulo.jsx
+++ WebTest/src/componentes/Titulo.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-function titulo (){
+function Titulo (){
 
 return(
     <>
