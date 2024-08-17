3114f80d608675b50973a5086e90a0bd8be8f9cb
angluq <79299906+angluq@users.noreply.github.com>
2024-08-17 14:01:46 +0000

    he creado el componente titulo

---
+++ WebTest/src/componentes/titulo.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+function titulo (){
+
+return(
+    <>
+    <h1>Conoce el perú primero</h1>
+    <h2>En el Perú existen hermososo lugares para conocer y disfrutar</h2>
+
+    </>
+)
+}
+
+
+export default titulo;
